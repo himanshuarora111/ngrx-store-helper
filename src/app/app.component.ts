edfdebcc2c0a542555d6b5dab3cc0f0fcf136871
himanshuarora111 <ha9813124245@gmail.com>
2025-06-23 18:36:34 +0530

    added test functionality for v2 of the library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SampleComponentComponent } from './components/sample-component/sample-component.component';
 import { DynamicComponentComponent } from './components/dynamic-component/dynamic-component.component';
-import { Store } from '@ngrx/store';
-import { ReducerManager } from '@ngrx/store';
+import { storeWrapper } from 'ngrx-store-wrapper';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SampleComponentComponent, DynamicComponentComponent],
+  imports: [RouterOutlet, CommonModule, SampleComponentComponent, DynamicComponentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ngrx-store-helper';
+  private counter = 1;
+  showDynamicComponent = true;
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+      storeWrapper.set('counter', this.counter);
+    }, 1000);
+  }
+
+  toggleDynamicComponent() {
+    this.showDynamicComponent = !this.showDynamicComponent;
+  }
 }
