8b6c422091ac778829e054fbd94212864b33197f
himanshuarora111 <ha9813124245@gmail.com>
2025-07-19 17:10:48 +0530

    added dev tools and upgraded version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,26 @@ import { routes } from './app.routes';
 import { getInitialDynamicReducers } from 'ngrx-store-wrapper';
 import { sampleReducer } from './components/sample-component/sample.reducer';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore({
-      ...getInitialDynamicReducers(), 
-      sampleReducer: sampleReducer
-    }),
-    provideHttpClient()
+    provideStore(
+      {
+        ...getInitialDynamicReducers(),
+        sample: sampleReducer
+      },
+    ),
+    provideHttpClient(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      name: 'ngrx-store-helper',
+      trace: true,
+      traceLimit: 25
+    })
   ]
 };
